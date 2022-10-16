fd6dc1840d45a0c5dcf39f338a6d5b3fd067be74
Raymond Ha <Haro@mail.uc.edu>
2022-10-16 00:40:32 -0400

    Added formulas for Craps Game

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void roll(String[] args)
+    {
+        Random rnd = new Random();
+        Scanner in = new Scanner(System.in);
+
+        boolean donePlay = true;
+        int diceRoll1=rnd.nextInt(6) + 1;
+        int diceRoll2=rnd.nextInt(6) + 1;
+        int crapsRolls= diceRoll1 + diceRoll2;
+        int thePoint = crapsRolls;
+
+        do {
+            if (crapsRolls == 2 || crapsRolls == 3 || crapsRolls == 12)
+            {
+                System.out.println("You crapped out. You lose!");
+            }
+            else if (crapsRolls == 7 || crapsRolls == 11)
+            {
+                System.out.println("You are naturally good. You win!");
+            }
+            else {
+                System.out.println("The point you got is " + crapsRolls + ". " + "That is your point.");
+                crapsRolls = thePoint;
+            }
+
+            while(crapsRolls != 7 && crapsRolls != thePoint)
+            {
+                if (crapsRolls == thePoint) {
+
+                }
+            }
+        }
+        while (!donePlay);
     }
 }
